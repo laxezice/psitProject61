54807168f6f414ee3f7e375355a0b9d9fd1715c8
laxezice <61070048@kmitl.ac.th>
2018-12-18 14:43:19 +0700

    edit generate.py and add graph

--- graph/generate.py
+++ graph/generate.py
@@ -124,6 +124,32 @@ with open('../database/Sell.txt') as csvfile:
         else:
             artist_sell[artist] = 1
 
+song_yt= {}
+artist_yt = {}
+
+with open('../database/YtChart2018.txt') as csvfile:
+    readfile = csv.reader(csvfile, delimiter=',')
+
+    for line in readfile:
+        song = str(line[0])
+        artist = str(line[1])
+
+        if "@" in song:
+            song = song.replace("@", ",")
+        if "@" in artist:
+            artist = artist.replace("@", ",")
+
+        # pick data into dictionary
+        if song in song_yt:
+            song_yt[song] += 1
+        else:
+            song_yt[song] = 1
+
+        if artist in artist_yt:
+            artist_yt[artist] += 1
+        else:
+            artist_yt[artist] = 1
+
 
 # sort data
 song_data = list(sorted(song_data.items(), key=lambda x: -x[1]))
@@ -134,6 +160,9 @@ song_stream = list(sorted(song_stream.items(), key=lambda x: -x[1]))
 artist_stream = list(sorted(artist_stream.items(), key=lambda x: -x[1]))
 style_stream = list(sorted(style_stream.items(), key=lambda x: -x[1]))
 artist_sell = list(sorted(artist_sell.items(), key=lambda x: -x[1]))
+song_yt= list(sorted(song_yt.items(), key=lambda x: -x[1]))
+artist_yt = list(sorted(artist_yt.items(), key=lambda x: -x[1]))
+
 
 # create chart
 line_chart = pygal.HorizontalBar()
@@ -183,3 +212,15 @@ line_chart.title = '1st artist best sell song 2018'
 for artist in artist_sell:
     line_chart.add(artist[0], artist[1])
 line_chart.render_to_file('artist_sell.svg')
+
+line_chart = pygal.HorizontalBar()
+line_chart.title = '1st artist best sell song 2018'
+for song in song_yt:
+    line_chart.add(song[0], song[1])
+line_chart.render_to_file('song_yt.svg')
+
+line_chart = pygal.HorizontalBar()
+line_chart.title = '1st artist best sell song 2018'
+for artist in artist_yt:
+    line_chart.add(artist[0], artist[1])
+line_chart.render_to_file('artist_yt.svg')
